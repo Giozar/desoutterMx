057948558613c6c4c960b73ba0ccb4c8f4c74674
Giovanni Cortazar <manuelgiozar04@gmail.com>
2023-10-18 02:13:02 -0600

    Intento de despliegue con output: server, netlify adapter, prueba 1

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import alpinejs from "@astrojs/alpinejs";
-
 import tailwind from "@astrojs/tailwind";
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://desouttermx.netlify.app/',
   base: '/',
-  // output: "server",
+  output: "server",
+  adapter: netlify(),
   integrations: [alpinejs(), tailwind()]
 });
