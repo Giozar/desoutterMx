8612d4b1420b070a70dce0c585cb860fc71766f0
Giovanni Cortazar <manuelgiozar04@gmail.com>
2023-11-17 11:39:49 -0600

    Implementación de api en QuoteForm funcional en herramientas

--- src/api/emailAPI.js
+++ src/api/emailAPI.js
@@ -1,4 +1,4 @@
 import axios from "./axios.js";
 
 export const contactRequest = async (data) => await axios.post("/contacto", data, {withCredentials: true});
-export const quoteRequest = async (data) => await axios.post("/contacto", data, {withCredentials: true});
+export const quoteRequest = async (url, data) => await axios.post(url, data, {withCredentials: true});
