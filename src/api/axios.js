5db99ca8a5950e9732f7f9ca23ee54b7ee12d103
Giovanni Cortazar <manuelgiozar04@gmail.com>
2023-11-17 12:32:40 -0600

    Implemetación de api en axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import {desoutterAPI} from "../config/config.js"
 // console.log(backendPort);
 const instance = axios.create({
-    baseURL: desoutterAPI,
+    baseURL: desoutterAPI || "https://7yc636gsjj.execute-api.us-east-1.amazonaws.com/prod",
     withCredentials: true
 })
 
